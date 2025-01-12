042b67b1f490153706e8022ee86e0d6ecd0cc81e
mireg <k.miranowski@gmail.com>
2025-01-12 17:30:23 +0100

    more estimators

--- src/models.py
+++ src/models.py
@@ -4,7 +4,31 @@ from lightgbm import LGBMRegressor
 
 def get_models(random_state=42):
     return {
-        'random_forest': RandomForestRegressor(n_estimators=100, random_state=random_state, n_jobs=-1),
-        'xgboost': xgb.XGBRegressor(random_state=random_state, n_jobs=-1),
-        'lightgbm': LGBMRegressor(random_state=random_state, n_jobs=-1)
+        'random_forest': RandomForestRegressor(
+            n_estimators=500,
+            max_depth=15,
+            min_samples_split=5,
+            min_samples_leaf=2,
+            max_features='sqrt',
+            random_state=random_state,
+            n_jobs=-1
+        ),
+        'xgboost': xgb.XGBRegressor(
+            n_estimators=500,
+            max_depth=7,
+            learning_rate=0.05,
+            subsample=0.8,
+            colsample_bytree=0.8,
+            random_state=random_state,
+            n_jobs=-1
+        ),
+        'lightgbm': LGBMRegressor(
+            n_estimators=500,
+            num_leaves=31,
+            learning_rate=0.05,
+            feature_fraction=0.8,
+            bagging_fraction=0.8,
+            random_state=random_state,
+            n_jobs=-1
+        )
     }
